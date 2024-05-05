7ea8435e4c2314a6c7449dcebc96aea9e8af0b6f
Sayem <sayem465508@gmail.com>
2024-05-05 07:48:35 +0600

    created the footer section

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import Home from "../Pages/Home/Home";
+import Root from "../Root/Root";
+
+import { createBrowserRouter } from 'react-router-dom'
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children:  [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
+
+export default routes;
